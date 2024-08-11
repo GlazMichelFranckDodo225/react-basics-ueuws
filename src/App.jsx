226144e73c49d4562283718aad60f328dffa2c55
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-11 07:50:37 +0200

    Hello from "Container" Component !

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Container from "./lessons/1.Le-JSX/Container"
+
 function App() {
 
   return (
     <div className="main-content">
- 
+      <h1>Root "App" Component</h1>
+      <Container />
     </div>
   )
 }
---
+++ src/lessons/1.Le-JSX/Container.jsx
@@ -0,0 +1,9 @@
+function Container() {
+  return (
+    <div>
+      <p>Hello from "Container" Component !</p>
+    </div>
+  )
+}
+
+export default Container
