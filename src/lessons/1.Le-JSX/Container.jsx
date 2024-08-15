fba4f5dae67fcd3bcf12d38acfa4db032435b986
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-15 07:53:55 +0200

    JSX Syntax and Features

--- src/lessons/1.Le-JSX/Container.jsx
+++ src/lessons/1.Le-JSX/Container.jsx
@@ -1,7 +1,36 @@
 function Container() {
+  const test = "Test";
+  function handleClick(e, id) {
+    console.log("Click 2", e, id);
+  }
+
   return (
     <div>
-      <p>Hello from "Container" Component !</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <ul>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+      </ul>
+      <button>Valider</button>
+      <p>{10 + 10}</p>
+      <p>{"ab" + "c"}</p>
+      <p>{"Le chat saute !".toUpperCase()}</p>
+      <p>{test}</p>
+      {/* Retourne tous les Eléments d'un Tableau (sauf les Objets) */}
+      <p>{[1, 2, 3, "k"]}</p>
+      <p>{[<span>Hello World !</span>]}</p>
+      <input type="text" />
+      <hr />
+      <img src="" alt="" />
+      <p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <label htmlFor=""></label>
+      <button aria-label="toggle button"></button>
+      <button onClick={() => console.log("Click")}>Click</button>
+      <br />
+      <button onClick={handleClick}>Click</button>
+      <br />
+      <button onClick={e => handleClick(e, 585)}>Click</button>
     </div>
   )
 }
